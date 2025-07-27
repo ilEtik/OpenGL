c37b6cbf549f889bb11351d6549723096f086e5a
ilEtik <elija.kaphengst.dev@pm.me>
2025-07-27 23:29:03 +0200

    drawing a beautiful shaded triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -4,54 +4,139 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << std::format("Failed to compile shader type: {0}\n", type);
+        std::cout << message << std::endl;
+
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexSource, const std::string& fragmentSource)
+{
+    unsigned int program = glCreateProgram();
+
+    unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER, vertexSource);
+    unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    if (vertexShader == 0 || fragmentShader == 0)
+    {
+        return 0;
+    }
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
 int main()
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
     {
         return -1;
     }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     GLenum glewInitResult = glewInit();
     if (glewInitResult != GLEW_OK)
     {
         std::cout << std::format("Glew Init failed with error: {0}\n", glewInitResult);
+        glfwTerminate();
         return -1;
     }
 
     std::cout << std::format("OpenGL Version: {0}\n", (const char*)glGetString(GL_VERSION));
 
-    /* Loop until the user closes the window */
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    std::string vertexSource = R"(
+		#version 330 core
+
+		layout(location = 0) in vec4 _position;
+
+		out vec4 v_Position;
+
+		void main()
+		{
+			v_Position = _position;
+			gl_Position = _position;
+		}
+	)";
+
+    std::string fragmentSource = R"(
+		#version 330 core
+
+		layout(location = 0) out vec4 _color;
+
+		in vec4 v_Position;
+
+		void main()
+		{
+			_color = v_Position + 0.5;
+		}
+	)";
+
+    unsigned int shader = CreateShader(vertexSource, fragmentSource);
+    glUseProgram(shader);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
